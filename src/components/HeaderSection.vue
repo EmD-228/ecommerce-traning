a4a59dbaa4421cd7d637b26924cb5069bf731fc5
Kokou DENYO <80518134+EmD-228@users.noreply.github.com>
2026-01-20 08:40:53 +0000

    ui: make HeaderSection responsive
    - Add responsive breakpoints for navigation menu
    - Hide menu on mobile, show hamburger icon
    - Adjust logo size for mobile (h-5 sm:h-10)
    - Hide action icons on small screens
    - Add Menu1Icon for mobile navigation

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,24 +1,25 @@
 <template>
-    <div class="navbar bg-base-100 shadow-sm px-8">
+    <div class="navbar bg-base-100 shadow-sm px-4">
         <div class="flex-1">
-            <img src="../assets/logos/logo.png" alt="logo" class="h-10">
+            <img src="../assets/logos/logo.png" alt="logo" class="h-5 sm:h-10 cursor-pointer">
         </div>
-        <div class="flex-1">
-            <ul class="menu menu-horizontal px-1 space-x-8">
+        <div class="hidden lg:flex md:flex-1">
+            <ul class="menu menu-horizontal px-1 space-x-2 sm:space-x-8">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Shop</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
         </div>
-        <div class="flex flex-row gap-4">
+        <div class="hidden sm:flex flex-row gap-4">
             <UserIcon class="text-black h-6 w-6" />
             <SearchNormal1Icon class="text-black h-6 w-6" />
             <HeartIcon class="text-black h-6 w-6" />
             <ShoppingCartIcon class="text-black h-6 w-6" />
         </div>
+        <Menu1Icon class="text-black sm:hidden h-6 w-6 cursor-pointer" />
     </div>
 </template>
 <script setup lang="ts">
-import { HeartIcon, SearchNormal1Icon, ShoppingCartIcon, UserIcon } from '@placetopay/iconsax-vue/outline';
+import { HeartIcon, Menu1Icon, SearchNormal1Icon, ShoppingCartIcon, UserIcon } from '@placetopay/iconsax-vue/outline';
 </script>
